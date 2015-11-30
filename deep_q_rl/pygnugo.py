cca9201933288d1148b19742dd0a40e341bf66fe
Siddarth Sampangi <siddarthsampangi@gmail.com>
2015-11-30 04:03:21 -0800

    Update pygnugo.py

--- deep_q_rl/pygnugo.py
+++ deep_q_rl/pygnugo.py
@@ -1,3 +1,5 @@
+import subprocess,os,fcntl
+
 proc = subprocess.Popen(['gnugo','--quiet','--mode','gtp'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
 proc.stdin.write('play black A3')
 fcntl.fcntl(proc.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
